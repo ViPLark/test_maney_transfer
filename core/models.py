bc89992b132f0f6f0264f38b84a88570a0e2694a
ViPLark <viplark@gmail.com>
2019-10-09 20:28:35 +0300

    Create user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    inn = models.IntegerField(db_index=True, help_text='ИНН')
+    amount = models.DecimalField(max_digits=8, decimal_places=2, help_text='Счёт')
+
+    @property
+    def amount_display(self):
+        return f'{self.amount} ₽'
