a0be8a3039442ee0b8e08ce03a2ff0d06e4ee35a
ViPLark <viplark@gmail.com>
2019-10-10 14:29:41 +0300

    Refactor outdoor money_sign

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .config import money_sign
+
 
 class User(AbstractUser):
     inn = models.IntegerField(db_index=True, help_text='ИНН')
@@ -8,4 +10,4 @@ class User(AbstractUser):
 
     @property
     def amount_display(self):
-        return f'{self.amount} ₽'
+        return f'{self.amount} {money_sign}'
